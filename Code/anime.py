707f79a4fc1631f5fa503539612bd9da5d894f75
unknown <fvendetta311@gmail.com>
2023-01-11 03:12:30 +0500

    added a user-agent

--- Code/anime.py
+++ Code/anime.py
@@ -6,9 +6,10 @@ class Anime:
         self.url = 'https://animego.org/'
         self.search_url = 'search/anime?q='
         self.random_url = 'anime/random'
+        self.user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     async def parse(self, url: str='') -> str:
-        async with self.session.get(self.url+url) as response:
+        async with self.session.get(self.url+url, headers=self.user_agent) as response:
             return await response.text()
 
     async def search(self, query: str) -> str:
