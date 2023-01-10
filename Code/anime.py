02bc3cb0e084a8c7bcceb727c3266e5f9509b521
unknown <fvendetta311@gmail.com>
2023-01-11 03:44:50 +0500

    fix bug with parser

--- Code/anime.py
+++ Code/anime.py
@@ -2,15 +2,17 @@ import aiohttp
 
 class Anime:
     def __init__(self) -> None:
-        self.session = aiohttp.ClientSession()
         self.url = 'https://animego.org/'
         self.search_url = 'search/anime?q='
         self.random_url = 'anime/random'
         self.user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     async def parse(self, url: str='') -> str:
+        session = aiohttp.ClientSession()
         async with self.session.get(self.url+url, headers=self.user_agent) as response:
-            return await response.text()
+            text = await response.text()
+        await session.close()
+        return text
 
     async def search(self, query: str) -> str:
         return await self.parse(self.search_url+'%20'.join(query.split()))
