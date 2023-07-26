88cee1321d64cc42383145bff855121d87f459f6
unknown <fvendetta311@gmail.com>
2023-07-26 11:12:52 +0500

    Пофиксил ошибку при выводе аниме из списка избранных

--- Code/commands_folder/commands/favorite_anime.py
+++ Code/commands_folder/commands/favorite_anime.py
@@ -10,10 +10,14 @@ async def favorite_anime(ctx):
     embed = discord.Embed()
     embed.title = 'Ваши избранные аниме'
 
-    desc = ''
+    if user_id not in db.users or not db.users[user_id]:
+        desc = 'Список пуст!'
 
-    for index, name_of_anime in enumerate(db.users[user_id]):
-        desc += f"{index+1}. {name_of_anime}\n"
+    else:
+        desc = ''
+        for index, name_of_anime in enumerate(db.users[user_id]):
+            desc += f"{index+1}. {name_of_anime}\n"
+    
 
     embed.description = desc
 
